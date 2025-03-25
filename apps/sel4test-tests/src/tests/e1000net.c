2259b9bf370360289bbd83cecd341b9ac3dc6f79
J1ngxuanWei <wjx021002@gmail.com>
2025-03-25 19:59:56 +0800

    1
    Signed-off-by: J1ngxuanWei <wjx021002@gmail.com>

--- apps/sel4test-tests/src/tests/e1000net.c
+++ apps/sel4test-tests/src/tests/e1000net.c
@@ -17,13 +17,42 @@
 static int
 test_e1000(env_t env)
 {
-    // First we scan pci to get device
+    // Scan pci to get device
     int error = sel4platsupport_new_io_ops(&env->vspace, &env->vka, &env->simple, &env->pci_inops);
     error = sel4platsupport_new_arch_ops(&env->pci_inops, &env->simple, &env->vka);
     error = sel4platsupport_new_io_ops(&env->vspace, &env->vka, &env->simple, &env->pci_outops);
     error = sel4platsupport_new_arch_ops(&env->pci_outops, &env->simple, &env->vka);
     libpci_scan(env->pci_inops.io_port_ops, env->pci_outops.io_port_ops);
-    // Then we will get 82574, vendor id = 8086, device id = 10d3
+
+    // Check the device
+    libpci_device_t* net = libpci_find_device(0x8086, 0x10d3);
+    if (!net)
+        return FAILURE;
+    
+    // Map device
+    uint64_t paddr = libpci_device_iocfg_get_baseaddr(&net->cfg, 0);
+    uint64_t size = net->cfg.base_addr_size[0];
+    uint64_t page_num = size / 4096;
+    void *vaddr_net;
+    uintptr_t cookie = 0;
+    reservation_t reserve = vspace_reserve_range_aligned(&env->vspace, page_num * BIT(12), 12, seL4_AllRights, 1, &vaddr_net);
+    seL4_CPtr frames[page_num];
+    for (int i = 0; i < page_num; i++)
+    {
+        vka_object_t frame;
+        int er = vka_alloc_frame_at(&env->vka, seL4_PageBits, paddr + i * 4096, &frame);
+        printf("----i : %i, er: %i\n", i, er);
+        frames[i] = frame.cptr;
+    }
+    int errpr = vspace_map_pages_at_vaddr(&env->vspace, frames, &cookie, (void *)vaddr_net, 32, 12, reserve);
+    printf("map, er: %i\n", errpr);
+
+    printf("=================================,paddr : %lx, size: %lx\n", (unsigned long)paddr, (unsigned long)size);
+    libpci_device_iocfg_debug_print(&libpci_find_device(0x8086, 0x10d3)->cfg, true);
+    printf("=================================\n");
+    libpci_device_iocfg_debug_print(&libpci_find_device(0x8086, 0x10d3)->cfg, false);
+    printf("=================================\n");
+    //error = vka_alloc_frame_at(&env.vka, seL4_PageBits, ut_paddr, &env.device_obj);
     return SUCCESS;
 }
 DEFINE_TEST(E1000NET0001, "Test e1000", test_e1000, true)
